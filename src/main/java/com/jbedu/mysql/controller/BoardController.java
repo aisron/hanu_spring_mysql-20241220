c26b468c3c755a49f992ae9ab9a3bcca0f03aa3b
JBEDU16 <JBEDU16@DESKTOP-HMF1SKG>
2024-12-20 11:36:02 +0900

    컨트롤러 게시판 목록 작성

--- src/main/java/com/jbedu/mysql/controller/BoardController.java
+++ src/main/java/com/jbedu/mysql/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.jbedu.mysql.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -7,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jbedu.mysql.dao.BoardDao;
+import com.jbedu.mysql.dto.BoardDto;
 
 @Controller
 public class BoardController {
@@ -25,6 +28,20 @@ public class BoardController {
 		boardDao.boardWrite(bname, btitle, bcontent);
 		
 		return "redirect:boardList"; // 게시판 리스트로 리다이렉트
+	}
+	
+	
+	@RequestMapping(value="/boardList")
+	public String boardList(HttpServletRequest request, Model model) {
+		
+		//모든 글 목록 가져와서 넘겨주면 
+		//가져오기
+		BoardDao boardDao = new BoardDao();
+		ArrayList<BoardDto> bDtos = boardDao.boardList(); // 모든 글 목록 -> 왜 모든글 목록인지 이해 필요
+		
+		model.addAttribute("bDtos", bDtos); 		
+		
+		return "boardList";
 	}
 	
 }
